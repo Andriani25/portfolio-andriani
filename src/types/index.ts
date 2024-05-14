9bd04b2a5016d0e6e7d8052cbcaf3300162800b6
Andriani25 <andriani.leandro25@gmail.com>
2024-05-13 21:27:12 -0300

    ts error

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,19 @@
 declare module '*.png' {
     const value: string;
-    //@ts-expect-error
     export = value;
   }
 
   declare module '*.jpeg' {
     const value: string;
-     //@ts-expect-error
     export = value;
   }
 
   declare module '*.webp' {
     const value: string;
-     //@ts-expect-error
     export = value;
   }
   
   declare module '*.jpg' {
     const value: string;
-     //@ts-expect-error
     export = value;
   }
